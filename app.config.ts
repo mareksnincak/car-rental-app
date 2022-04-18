115acf42fb5ee93a74fb89490cc4ad9f37cd65d3
Marek Snincak <m.snincak@gmail.com>
2022-04-18 15:00:07 +0200

    feat: versioning

--- app.config.ts
+++ app.config.ts
@@ -4,7 +4,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: "Car rental",
   slug: "car-rental",
-  version: "1.1.4",
+  version: "1.2.0",
   orientation: "portrait",
   icon: "./src/assets/images/icon.png",
   scheme: "myapp",
